9946b5954ed771781b15fe0884cbf7f36d4d0767
Artem <Styoganov@bk.ru>
2022-08-06 11:52:16 +0300

    task0

---
+++ task_0/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Напишите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Напашите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+if (num1 > num2)
+{
+    Console.Write($"Max = {num1} ");
+}
+else 
+{
+    Console.Write($"Max = {num2}");
+}
