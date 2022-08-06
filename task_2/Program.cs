ef9276845eef16800d417d4cfa9630cb3f57d383
Artem <Styoganov@bk.ru>
2022-08-06 13:03:15 +0300

    task2

---
+++ task_2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num % 2 == 0)
+{
+    Console.Write("Число является четным");
+}
+else 
+{
+    Console.Write("Число не является четным");
+}
